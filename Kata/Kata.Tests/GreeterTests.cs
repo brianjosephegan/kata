84bec82ea2b7b93e3b884dba57fb2c4a89cbe1dc
Brian Egan <brianjoseph.egan@gmail.com>
2021-03-25 18:34:35 -0700

    Greeter Kata
    Implement a greeting mechanism with unit tests.

---
+++ Kata/Kata.Tests/GreeterTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class GreeterTests
+    {
+        private readonly Greeter greeter = new Greeter();
+
+        [TestCase("Welcome", "english")]
+        [TestCase("Welkom", "dutch")]
+        [TestCase("Welcome", "IP_ADDRESS_INVALID")]
+        [TestCase("Welcome", "")]
+        [TestCase("Welcome", "2")]
+        public void Test(string expected, string input)
+        {
+            Assert.AreEqual(expected, greeter.Greet(input));
+        }
+    }
+}
---
+++ Kata/Kata/Greeter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class Greeter
+    {
+        private readonly string defaultLanguage = "english";
+
+        private readonly Dictionary<string, string> languageToGreetingMaps = new Dictionary<string, string>()
+        {
+            { "english", "Welcome" },
+            { "czech", "Vitejte" },
+            { "danish", "Velkomst" },
+            { "dutch", "Welkom" },
+            { "estonian", "Tere tulemast" },
+            { "finnish", "Tervetuloa" },
+            { "flemish", "Welgekomen" },
+            { "french", "Bienvenue" },
+            { "german", "Willkommen" },
+            { "irish", "Failte" },
+            { "italian", "Benvenuto" },
+            { "latvian", "Gaidits" },
+            { "lithuanian", "Laukiamas" },
+            { "polish", "Witamy" },
+            { "spanish", "Bienvenido" },
+            { "swedish", "Valkommen" },
+            { "welsh", "Croeso" }
+        };
+
+        public string Greet(string language)
+        {
+            if (languageToGreetingMaps.ContainsKey(language))
+            {
+                return languageToGreetingMaps[language];
+            }
+            else
+            {
+                return languageToGreetingMaps[defaultLanguage];
+            }
+        }
+    }
+}
