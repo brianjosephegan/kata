2bdaea496c96559ff2b72de2ee75de00a1e3e33d
Brian Egan <brianjoseph.egan@gmail.com>
2021-04-10 19:20:08 -0700

    Binary Tree Level Sorter
    Implement a level sorter for a binary tree.

---
+++ Kata.Tests/BinaryTreeLevelSorterTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static Kata.BinaryTreeLevelSorter;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public sealed class BinaryTreeLevelSorterTests
+    {
+        private readonly BinaryTreeLevelSorter binaryTreeLevelSorter = new BinaryTreeLevelSorter();
+
+        [Test]
+        public void NullTest()
+        {
+            Assert.AreEqual(
+                new List<int>(),
+                binaryTreeLevelSorter.Sort(null));
+        }
+
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(
+                new List<int>() { 1, 2, 3, 4, 5, 6 },
+                binaryTreeLevelSorter.Sort(new Node(new Node(null, new Node(null, null, 4), 2), new Node(new Node(null, null, 5), new Node(null, null, 6), 3), 1)));
+        }
+    }
+}
---
+++ Kata/BinaryTreeLevelSorter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public sealed class BinaryTreeLevelSorter
+    {
+        public class Node
+        {
+            public Node Left { get; }
+            public Node Right { get; }
+            public int Value { get; }
+
+            public Node(Node left, Node right, int value)
+            {
+                Left = left;
+                Right = right;
+                Value = value;
+            }
+        }
+
+        public List<int> Sort(Node node)
+        {
+            List<int> result = new List<int>();
+            Queue<Node> nodesToVisit = new Queue<Node>();
+
+            if (node != null)
+            {
+                nodesToVisit.Enqueue(node);
+            }
+
+            while (nodesToVisit.Any())
+            {
+                var currentNode = nodesToVisit.Dequeue();
+
+                result.Add(currentNode.Value);
+
+                if (currentNode.Left != null)
+                {
+                    nodesToVisit.Enqueue(currentNode.Left);
+                }
+                if (currentNode.Right != null)
+                {
+                    nodesToVisit.Enqueue(currentNode.Right);
+                }
+            }
+
+            return result;
+        }
+    }
+}
