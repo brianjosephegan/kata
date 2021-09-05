2d1f99ecc91b994d73e3d439579486d05fbfd1e8
Brian Egan <brianjoseph.egan@gmail.com>
2021-09-05 13:46:47 -0700

    Longest Repetitive Character
    Implement algorithm to find the longest repeating character in a string.

---
+++ Kata.Tests/LongestRepetitiveCharacterFinderTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class LongestRepetitiveCharacterFinderTests
+    {
+        private readonly LongestRepetitiveCharacterFinder longestRepetitiveCharacterFinder = new LongestRepetitiveCharacterFinder();
+
+        [Test]
+        public void LongestAtTheBeginningTest()
+        {
+            Assert.AreEqual(new Tuple<char?, int>('a', 4), longestRepetitiveCharacterFinder.Find("aaaabb"));
+            Assert.AreEqual(new Tuple<char?, int>('b', 5), longestRepetitiveCharacterFinder.Find("abbbbb"));
+        }
+
+        [Test]
+        public void LongestAtTheEndTest()
+        {
+            Assert.AreEqual(new Tuple<char?, int>('a', 4), longestRepetitiveCharacterFinder.Find("bbbaaabaaaa"));
+        }
+
+        [Test]
+        public void LongestInTheMiddleTest()
+        {
+            Assert.AreEqual(new Tuple<char?, int>('u', 3), longestRepetitiveCharacterFinder.Find("cbdeuuu900"));
+        }
+
+        [Test]
+        public void MultipleLongestTest()
+        {
+            Assert.AreEqual(new Tuple<char?, int>('a', 2), longestRepetitiveCharacterFinder.Find("aabb"));
+            Assert.AreEqual(new Tuple<char?, int>('b', 1), longestRepetitiveCharacterFinder.Find("ba"));
+        }
+
+        [Test]
+        public void EmptyStringTest()
+        {
+            Assert.AreEqual(new Tuple<char?, int>(null, 0), longestRepetitiveCharacterFinder.Find(""));
+        }
+    }
+}
---
+++ Kata/LongestRepetitiveCharacterFinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class LongestRepetitiveCharacterFinder
+    {
+        public Tuple<char?, int> Find(string input)
+        {
+            var maxCharToCount = new Tuple<char?, int>(null, 0);
+
+            char? currentChar = null;
+            int currentCharCount = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == 0)
+                {
+                    currentChar = input[i];
+                }
+
+                if (currentChar == input[i])
+                {
+                    currentCharCount++;
+                }
+                else
+                {
+                    if (currentCharCount > maxCharToCount.Item2)
+                    {
+                        maxCharToCount = new Tuple<char?, int>(currentChar, currentCharCount);
+                    }
+
+                    currentChar = input[i];
+                    currentCharCount = 1;
+                }
+
+                if (i == input.Length - 1 && currentCharCount > maxCharToCount.Item2)
+                {
+                    maxCharToCount = new Tuple<char?, int>(currentChar, currentCharCount);
+                }
+            }
+
+            return maxCharToCount;
+        }
+    }
+}
