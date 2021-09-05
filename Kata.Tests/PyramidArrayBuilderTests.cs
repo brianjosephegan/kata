2a5e308fb4b93db3393409345280c03fe8fd4cf4
Brian Egan <brianjoseph.egan@gmail.com>
2021-09-05 09:14:06 -0700

    Pyramid Builder
    Implement algorithm to create a pyramid of n levels.

---
+++ Kata.Tests/PyramidArrayBuilderTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class PyramidArrayBuilderTests
+    {
+        private readonly PyramidArrayBuilder pyramidArrayBuilder = new PyramidArrayBuilder();
+
+        [TestCaseSource(nameof(TestData))]
+        public void BasicTests(int levels, int[][] expected)
+        {
+            var actual = pyramidArrayBuilder.Build(levels);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        private static List<object[]> TestData
+        {
+            get
+            {
+                return new List<object[]>
+                {
+                    new object[]
+                    {
+                        0,
+                        new int[][] {}
+                    },
+                    new object[]
+                    {
+                        1,
+                        new int[][] { new int[] { 1 } }
+                    },
+                    new object[]
+                    {
+                        2,
+                        new int[][] { new int[] { 1 }, new int[] { 1, 1 } }
+                    },
+                    new object[]
+                    {
+                        3,
+                        new int[][] { new int[] { 1 }, new int[] { 1, 1 }, new int[] { 1, 1, 1 } }
+                    }
+                };
+            }
+        }
+    }
+}
---
+++ Kata/PyramidArrayBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class PyramidArrayBuilder
+    {
+        public int[][] Build(int levels)
+        {
+            int[][] result = new int[levels][];
+
+            for (int i = 0; i < levels; i++)
+            {
+                result[i] = Enumerable
+                    .Repeat(1, i + 1)
+                    .ToArray();
+            }
+
+            return result;
+        }
+    }
+}
