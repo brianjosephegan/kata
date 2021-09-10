ea07cbca2e03a80cc3a76ab37c3024941105a171
Brian Egan <brianjoseph.egan@gmail.com>
2021-09-09 21:57:27 -0700

    Highest Scoring Word Finder
    Implement algorithm to find the highest scoring word in a sentence.

---
+++ Kata.Tests/HighestScoringWordFinderTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Kata.Tests
+{
+    [TestFixture]
+    public class HighestScoringWordFinderTests
+    {
+        private HighestScoringWordFinder _highestScoringWordFinder = new HighestScoringWordFinder();
+
+        [TestCase("man i need a taxi up to ubud", "taxi")]
+        [TestCase("what time are we climbing up to the volcano", "volcano")]
+        [TestCase("take me to semynak", "semynak")]
+        [TestCase("aa b", "aa")]
+        [TestCase("b aa", "b")]
+        [TestCase("bb d", "bb")]
+        [TestCase("d bb", "d")]
+        [TestCase("aaa b", "aaa")]
+        public void Test(string input, string expectedWord)
+        {
+            var actualWord = _highestScoringWordFinder.Find(input);
+
+            Assert.AreEqual(expectedWord, actualWord);
+        }
+    }
+}
---
+++ Kata/HighestScoringWordFinder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class HighestScoringWordFinder
+    {
+        public string Find(string input)
+        {
+            string highWord = null;
+            int highWordScore = 0;
+
+            foreach (var word in input.Split(' '))
+            {
+                var wordScore = GetWordScore(word);
+                if (wordScore > highWordScore)
+                {
+                    highWord = word;
+                    highWordScore = wordScore;
+                }
+            }
+
+            return highWord;
+        }
+
+        private int GetWordScore(string word) => word.Select(c => c % 32).Sum();
+    }
+}
