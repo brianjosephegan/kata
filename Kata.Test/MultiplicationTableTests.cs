216dcbf22ecad95b865c34b2cdb3eb6b01dd54a8
Brian Egan <brianjoseph.egan@gmail.com>
2022-10-02 14:37:34 -0700

    Multiplication Table
    Add Kata and unit tests for method to create a multiplication table based on a given integer.

---
+++ Kata.Test/MultiplicationTableTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Kata.Test
+{
+    public class MultiplicationTableTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var expected = new int[,]
+            {
+                { 1, 2, 3 },
+                { 2, 4, 6 },
+                { 3, 6, 9 }
+            };
+
+            var result = MultiplicationTable.Create(3);
+
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ Kata/MultiplicationTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public static class MultiplicationTable
+    {
+        public static int[,] Create(int size)
+        {
+            var result = new int[size,size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    result[i, j] = (j + 1) * (i + 1);
+                }
+            }
+
+            return result;
+        }
+    }
+}
