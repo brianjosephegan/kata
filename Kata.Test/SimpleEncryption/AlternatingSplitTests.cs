d736d9dd7b1f4f24f16e5bc4ead1500cd087a513
Brian Egan <brianjoseph.egan@gmail.com>
2022-10-10 10:00:40 -0700

    Alternating Split Encryption
    Implement simple encryption algorithm using alternating split.

---
+++ Kata.Test/SimpleEncryption/AlternatingSplitTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Kata.SimpleEncryption;
+using Xunit;
+
+namespace Kata.Test.SimpleEncryption
+{
+    public class AlternatingSplitTests
+    {
+        [Theory]
+        [InlineData("This is a test!", 0, "This is a test!")]
+        [InlineData("This is a test!", 1, "hsi  etTi sats!")]
+        [InlineData("This is a test!", 2, "s eT ashi tist!")]
+        [InlineData("This is a test!", 3, " Tah itse sits!")]
+        [InlineData("This is a test!", 4, "This is a test!")]
+        [InlineData("This is a test!", -1, "This is a test!")]
+        [InlineData("This kata is very interesting!", 1, "hskt svr neetn!Ti aai eyitrsig")]
+        public void EncryptExampleTests(string input, int n, string expected)
+        {
+            AlternatingSplit.Encrypt(input, n).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("This is a test!", 0, "This is a test!")]
+        [InlineData("hsi  etTi sats!", 1, "This is a test!")]
+        [InlineData("s eT ashi tist!", 2, "This is a test!")]
+        [InlineData(" Tah itse sits!", 3, "This is a test!")]
+        [InlineData("This is a test!", 4, "This is a test!")]
+        [InlineData("This is a test!", -1, "This is a test!")]
+        [InlineData("hskt svr neetn!Ti aai eyitrsig", 1, "This kata is very interesting!")]
+        public void DecryptExampleTests(string input, int n, string expected)
+        {
+            AlternatingSplit.Decrypt(input, n).Should().Be(expected);
+        }
+
+        [Fact]
+        public void EmptyTests()
+        {
+            AlternatingSplit.Encrypt(string.Empty, 0).Should().BeEmpty();
+            AlternatingSplit.Decrypt(string.Empty, 0).Should().BeEmpty();
+        }
+
+        [Fact]
+        public void NullTests()
+        {
+            AlternatingSplit.Encrypt(null, 0).Should().BeNull();
+            AlternatingSplit.Decrypt(null, 0).Should().BeNull();
+        }
+    }
+}
---
+++ Kata/SimpleEncryption/AlternatingSplit.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Kata.SimpleEncryption
+{
+    public static class AlternatingSplit
+    {
+        public static string Encrypt(string text, int n)
+        {
+            if (string.IsNullOrWhiteSpace(text) || n <= 0)
+            {
+                return text;
+            }
+
+            string result = text;
+
+            for (int i = 0; i < n; i++)
+            {
+                result = string.Concat(
+                    new string(result.Where((c, i) => i % 2 != 0).ToArray()),
+                    new string(result.Where((c, i) => i % 2 == 0).ToArray()));
+            }
+
+            return result;
+        }
+
+        public static string Decrypt(string encryptedText, int n)
+        {
+            if (string.IsNullOrWhiteSpace(encryptedText) || n <= 0)
+            {
+                return encryptedText;
+            }
+
+            string result = encryptedText;
+
+            for (int i = 0; i < n; i++)
+            {
+                var firstHalf = result.Where((c, i) => i < result.Length / 2);
+                var secondHalf = result.Where((c, i) => i >= result.Length / 2);
+                var stringBuilder = new StringBuilder();
+
+                using (var firstHalfEnumerator = firstHalf.GetEnumerator())
+                using (var secondHalfEnumerator = secondHalf.GetEnumerator())
+                {
+                    while (stringBuilder.Length != result.Length)
+                    {
+                        if (secondHalfEnumerator.MoveNext())
+                        {
+                            stringBuilder.Append(secondHalfEnumerator.Current);
+                        }
+
+                        if (firstHalfEnumerator.MoveNext())
+                        {
+                            stringBuilder.Append(firstHalfEnumerator.Current);
+                        }
+                    }
+                }
+
+                result = stringBuilder.ToString();
+            }
+
+            return result;
+        }
+    }
+}
