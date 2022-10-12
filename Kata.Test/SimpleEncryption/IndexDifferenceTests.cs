f76a63ed03e7705db6b1822db889c31fa64811c5
Brian Egan <brianjoseph.egan@gmail.com>
2022-10-11 19:41:11 -0700

    Index Difference
    Implement simple encryption algorithm using Index Difference.

---
+++ Kata.Test/SimpleEncryption/IndexDifferenceTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Kata.SimpleEncryption;
+using Xunit;
+
+namespace Kata.Test.SimpleEncryption
+{
+    public class IndexDifferenceTests
+    {
+        [Theory]
+        [InlineData("Do the kata \"Kobayashi-Maru-Test!\" Endless fun and excitement when finding a solution!", "$-Wy,dM79H'i'o$n0C&I.ZTcMJw5vPlZc Hn!krhlaa:khV mkL;gvtP-S7Rt1Vp2RV:wV9VuhO Iz3dqb.U0w")]
+        [InlineData("This is a test!", "5MyQa9p0riYplZc")]
+        [InlineData("This kata is very interesting!", "5MyQa79H'ijQaw!Ns6jVtpmnlZ.V6p")]
+        public void EncryptExampleTests(string input, string expected)
+        {
+            IndexDifference.Encrypt(input).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("$-Wy,dM79H'i'o$n0C&I.ZTcMJw5vPlZc Hn!krhlaa:khV mkL;gvtP-S7Rt1Vp2RV:wV9VuhO Iz3dqb.U0w", "Do the kata \"Kobayashi-Maru-Test!\" Endless fun and excitement when finding a solution!")]
+        [InlineData("5MyQa9p0riYplZc", "This is a test!")]
+        [InlineData("5MyQa79H'ijQaw!Ns6jVtpmnlZ.V6p", "This kata is very interesting!")]
+        public void DecryptExampleTests(string input, string expected)
+        {
+            IndexDifference.Decrypt(input).Should().Be(expected);
+        }
+
+        [Fact]
+        public void EmptyTests()
+        {
+            IndexDifference.Encrypt(string.Empty).Should().BeEmpty();
+            IndexDifference.Decrypt(string.Empty).Should().BeEmpty();
+        }
+
+        [Fact]
+        public void NullTests()
+        {
+            IndexDifference.Encrypt(null).Should().BeNull();
+            IndexDifference.Decrypt(null).Should().BeNull();
+        }
+    }
+}
---
+++ Kata/SimpleEncryption/IndexDifference.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+
+namespace Kata.SimpleEncryption
+{
+    public static class IndexDifference
+    {
+        private const string CharRegion = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        private const string DigitRegion = "0123456789";
+        private const string SpecialRegion = ".,:;-?! '()$%&\"";
+
+        private static readonly string Region = $"{CharRegion}{DigitRegion}{SpecialRegion}";
+
+        public static string Encrypt(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return text;
+            }
+
+            var result = text.ToCharArray();
+
+            if (result.Any(c => Region.IndexOf(c) < 0))
+            {
+                throw new Exception();
+            }
+
+            for (int i = 1; i < result.Length; i += 2)
+            {
+                if (char.IsUpper(result[i]))
+                {
+                    result[i] = char.ToLower(result[i]);
+                }
+                else
+                {
+                    result[i] = char.ToUpper(result[i]);
+                }
+            }
+
+            var step1 = new string(result);
+
+            for (int i = 0; i < result.Length - 1; i++)
+            {
+                var index = Region.IndexOf(step1[i]) - Region.IndexOf(step1[i + 1]);
+                result[i + 1] = Region[(index + Region.Length) % Region.Length];
+            }
+            
+            result[0] = Region[Region.Length - 1 - Region.IndexOf(result[0])];
+
+            return new string(result);
+        }
+
+        public static string Decrypt(string encryptedText)
+        {
+            if (string.IsNullOrEmpty(encryptedText))
+            {
+                return encryptedText;
+            }
+
+            var result = encryptedText.ToCharArray();
+
+            result[0] = Region[Region.Length - Region.IndexOf(result[0]) - 1];
+
+            for (int i = 0; i < result.Length - 1; i++)
+            {
+                var index = Region.IndexOf(result[i]) - Region.IndexOf(result[i + 1]);
+                result[i + 1] = Region[(index + Region.Length) % Region.Length];
+            }
+
+            for (int i = 1; i < result.Length; i += 2)
+            {
+                if (char.IsUpper(result[i]))
+                {
+                    result[i] = char.ToLower(result[i]);
+                }
+                else
+                {
+                    result[i] = char.ToUpper(result[i]);
+                }
+            }
+
+            return new string(result);
+        }
+    }
+}
