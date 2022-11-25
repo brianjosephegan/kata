50aa6abe5d1f61e5bbc4a143548b58317f1fca8f
Brian Egan <brianjoseph.egan@gmail.com>
2022-11-24 22:08:06 -0800

    Vowel Count
    Implement vowel count kata.

---
+++ Kata.Test/VowelCountTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Kata.Test
+{
+    public class VowelCountTests
+    {
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("a", 1)]
+        [InlineData("u2978u", 2)]
+        [InlineData("bb", 0)]
+        [InlineData("aeiou", 5)]
+        public void Test(string input, int expected)
+        {
+            var result = VowelCount.GetVowelCount(input);
+
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ Kata/VowelCount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace Kata
+{
+    public static class VowelCount
+    {
+        private static char[] Vowels = new[] { 'a', 'e', 'i', 'o', 'u' };
+
+        public static int GetVowelCount(string input) => input.Count(c => Vowels.Contains(c));
+    }
+}
