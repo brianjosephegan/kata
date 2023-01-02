0d6a8ccf45ee5c1f46e61c3a4c78e74ca4ac9bd1
Brian Egan <brianjoseph.egan@gmail.com>
2023-01-01 20:07:57 -0800

    Multiples of 3 and 5
    Implement algorithm and tests to sum all natural numbers that are evenly divisible by either 3 or 5.

---
+++ Kata.Test/MultiplesOf3And5Tests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Kata.Test
+{
+    public class MultiplesOf3And5Tests
+    {
+        [Fact]
+        public void Test()
+        {
+            MultiplesOf3And5.Get(10).Should().Be(23);
+        }
+    }
+}
---
+++ Kata/MultiplesOf3And5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class MultiplesOf3And5
+    {
+        public static int Get(int value)
+        {
+            return Enumerable
+                .Range(0, value)
+                .Where(i => i % 3 == 0 || i % 5 == 0)
+                .Sum();
+        }
+    }
+}
