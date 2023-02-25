46f9ac5f278289a4e3cf19bf07b72c66a7576c66
Brian Egan <brianjoseph.egan@gmail.com>
2023-02-25 11:14:41 -0800

    High And Low
    Implement algorithm to format a string with highest and lowest numbers from an input string of space-separated numbers.

---
+++ Kata.Test/HighAndLowTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Kata.Test
+{
+    public class HighAndLowTests
+    {
+        [Theory]
+        [InlineData("1 2 3", "3 1")]
+        [InlineData("8 3 -5 42 -1 0 0 -9 4 7 4 -4", "42 -9")]
+        public void Test(string input, string expected)
+        {
+            HighAndLow.Get(input).Should().Be(expected);
+        }
+    }
+}
---
+++ Kata/HighAndLow.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Kata
+{
+    public class HighAndLow
+    {
+        public static string Get(string input)
+        {
+            var numbers = input.Split(" ").Select(s => int.Parse(s));
+            return $"{numbers.Max()} {numbers.Min()}";
+        }
+    }
+}
