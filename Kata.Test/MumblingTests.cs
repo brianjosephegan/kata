b1bd53cbd22f470c13a0e0d37c1407c3ee8613a2
Brian Egan <brianjoseph.egan@gmail.com>
2023-02-26 12:24:11 -0800

    Mumbling
    Implement Mumbling Kata.

---
+++ Kata.Test/MumblingTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Kata.Test
+{
+    public class MumblingTests
+    {
+        [Theory]
+        [InlineData("ZpglnRxqenU", "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")]
+        [InlineData("NyffsGeyylB", "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")]
+        [InlineData("MjtkuBovqrU", "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")]
+        [InlineData("EvidjUnokmM", "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")]
+        [InlineData("HbideVbxncC", "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")]
+        public void Test(string input, string expected)
+        {
+            Mumbling.Mumble(input).Should().Be(expected);
+        }
+    }
+}
---
+++ Kata/Mumbling.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace Kata
+{
+    public class Mumbling
+    {
+        public static string Mumble(string input)
+        {
+            return string.Join(
+                "-",
+                Enumerable.Range(0, input.Length).Select(i => $"{char.ToUpper(input[i])}{new string(char.ToLower(input[i]), i)}"));
+        }
+    }
+}
